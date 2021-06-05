1ab6d9802aa54df371ee4af9aa93bae2a71d7d2f
Abhi-2008 <71825512+Abhi-2008@users.noreply.github.com>
2021-06-05 16:49:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 noseX = 0;
+function preload(){
 
+}
 function setup() {
+    canvas=createCanvas(400,400);
+    canvas.position(560,130);
     video = createCapture(VIDEO);
     video.size(550, 500)
-    canvas = createCanvas(400, 400);
-    canvas.position(560, 150);
     poseNet = ml5.poseNet('video', modelLoaded);
     poseNet.on('pose', gotPoses);
 }
